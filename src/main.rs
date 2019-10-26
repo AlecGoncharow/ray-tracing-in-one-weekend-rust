34de6ad068dabb5ebca2cf0809c58c9263d2d9bf
Alec Goncharow <Alec.Goncharow@gmail.com>
2019-10-26 00:13:46 -0400

    chapter 4 done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+mod ray;
+use ray::Ray;
 mod vec;
 use vec::Vec3;
 
@@ -33,6 +35,27 @@ struct Color {
     b: u8,
 }
 
+impl Color {
+    fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    fn from_vec3(vec: Vec3) -> Self {
+        Self::new(vec.x as u8, vec.y as u8, vec.z as u8)
+    }
+
+    fn from_normalized_vec3(vec: Vec3) -> Self {
+        Self::from_vec3(vec * 255.99)
+    }
+}
+
+fn color(ray: Ray) -> Color {
+    let unit_direction = ray.direction.make_unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    let temp_color = (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0);
+    Color::from_normalized_vec3(temp_color)
+}
+
 fn main() -> std::io::Result<()> {
     let mut out = Output {
         rows: 250,
@@ -40,19 +63,19 @@ fn main() -> std::io::Result<()> {
         colors: vec![],
     };
 
-    for i in 0..out.rows {
-        for j in 0..out.cols {
-            let normalized_colors = Vec3::new(
-                j as f32 / out.cols as f32,
-                (out.rows - i) as f32 / out.rows as f32,
-                0.2f32,
-            );
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, -0.0);
 
-            let color = Color {
-                r: (normalized_colors.x * 255.99) as u8,
-                g: (normalized_colors.y * 255.99) as u8,
-                b: (normalized_colors.z * 255.99) as u8,
-            };
+    for j in 0..out.rows {
+        for i in 0..out.cols {
+            let u = i as f32 / out.cols as f32;
+            let v = (out.rows - j) as f32 / out.cols as f32;
+
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+
+            let color = color(ray);
 
             out.colors.push(color);
         }
